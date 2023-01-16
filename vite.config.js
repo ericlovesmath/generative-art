6cfc8871451b86942deae9b02bef4b59324569a4
ericlovesmath <ericlovesmath@gmail.com>
2023-01-15 21:01:04 -0800

    I give up dealing with Github Pages routing.

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,20 @@
-// import { resolve } from "path";
+import { resolve } from "path";
 import { defineConfig } from "vite";
 
+const root = resolve(__dirname, "src");
+const outDir = resolve(__dirname, "dist");
+
 export default defineConfig({
-  base: "./",
-  // build: {
-  //   rollupOptions: {
-  //     input: {
-  //       main: resolve(__dirname, "index.html"),
-  //       laser_simulation: resolve(__dirname, "src/laser-simulation/index.html"),
-  //       generative_art: resolve(__dirname, "src/generative-art/index.html"),
-  //     },
-  //   },
-  // },
+  root: root,
+  build: {
+    outDir: outDir,
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(root, "index.html"),
+        laser_simulation: resolve(root, "laser-simulation", "index.html"),
+        generative_art: resolve(root, "generative-art", "index.html"),
+      },
+    },
+  },
 });
