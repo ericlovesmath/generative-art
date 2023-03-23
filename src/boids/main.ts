82d25392d52ab44c5922b10f924bda5e182dffd2
ericlovesmath <ericlovesmath@gmail.com>
2023-03-23 15:41:17 -0700

    [Boids] Add Sliders to adjust constants, modify Boids to be independent of canvas

--- src/boids/main.ts
+++ src/boids/main.ts
@@ -2,10 +2,44 @@ import "./style.css";
 import { CanvasBoard } from "../lib/canvas";
 import { Flock } from "./objects/flock";
 
+let coheranceSlider =
+  document.querySelector<HTMLInputElement>("#coherance-slider")!;
+let coheranceOutput =
+  document.querySelector<HTMLOutputElement>("#coherance-output")!;
+let alignmentSlider =
+  document.querySelector<HTMLInputElement>("#alignment-slider")!;
+let alignmentOutput =
+  document.querySelector<HTMLOutputElement>("#alignment-output")!;
+let separationSlider =
+  document.querySelector<HTMLInputElement>("#separation-slider")!;
+let separationOutput =
+  document.querySelector<HTMLOutputElement>("#separation-output")!;
+let restartButton =
+  document.querySelector<HTMLButtonElement>("#restart-button")!;
+
 const playground = new CanvasBoard(
   document.querySelector("#canvas-board")!,
   960,
-  500
+  420
 );
 
-new Flock(playground, 100);
+let flock = new Flock(playground, 100);
+
+coheranceSlider.addEventListener("input", () => {
+  coheranceOutput.textContent = coheranceSlider.value;
+  flock.COHERANCE = (0.0005 * coheranceSlider.valueAsNumber) / 50;
+});
+
+alignmentSlider.addEventListener("input", () => {
+  alignmentOutput.textContent = alignmentSlider.value;
+  flock.ALIGNMENT = (0.05 * alignmentSlider.valueAsNumber) / 50;
+});
+
+separationSlider.addEventListener("input", () => {
+  separationOutput.textContent = separationSlider.value;
+  flock.SEPARATION = (0.01 * separationSlider.valueAsNumber) / 50;
+});
+
+restartButton.addEventListener("click", () => {
+  flock.randomizeBoids();
+});
--- src/boids/objects/flock.ts
+++ src/boids/objects/flock.ts
@@ -1,130 +1,153 @@
 import { Vec, mod } from "../../lib/utils";
 import { CanvasBoard, CanvasObject, PeriodicDrawer } from "../../lib/canvas";
 
-class Boid extends CanvasObject {
+class Boid {
   pos: Vec;
   vel: Vec;
   acc: Vec;
+
   RAD = 7;
 
-  MAX_FORCE = 0.05;
-
-  constructor(board: CanvasBoard, pos: Vec, vel: Vec) {
-    super(board);
+  constructor(pos: Vec, vel: Vec) {
     this.pos = pos;
     this.vel = vel;
     this.acc = new Vec(0, 0);
   }
 
-  /** Flies towards average position of local boids */
-  cohere(locals: Boid[], coherance: number) {
-    if (locals.length == 0) {
-      return;
-    }
+  /** Returns steering force to fly towards average position of local boids */
+  cohere(locals: Boid[]): Vec {
     let avgPosition = new Vec(0, 0);
     for (let curr of locals) {
       avgPosition = avgPosition.add(curr.pos);
     }
-    avgPosition = avgPosition.scale(1 / locals.length);
-    let steering = avgPosition.subtract(this.pos).scale(coherance);
-    this.acc = this.acc.add(steering.clamp(this.MAX_FORCE));
+    if (locals.length == 0) {
+      return avgPosition;
+    }
+    return avgPosition.scale(1 / locals.length).subtract(this.pos);
   }
 
-  /** Steers to avoid crowding local flock */
-  separate(locals: Boid[], separation: number) {
-    if (locals.length == 0) {
-      return;
-    }
+  /** Returns steering force to avoid crowding local flock */
+  separate(locals: Boid[]): Vec {
     let target = new Vec(0, 0);
     for (let curr of locals) {
       let diff = this.pos.subtract(curr.pos);
       diff.scale(1 / diff.magnitude() ** 2);
       target = target.add(diff);
     }
-    target = target.scale(1 / locals.length);
-    let steering = target.subtract(this.vel).scale(separation);
-    this.acc = this.acc.add(steering.clamp(this.MAX_FORCE));
+    if (locals.length == 0) {
+      return target;
+    }
+    return target.scale(1 / locals.length).subtract(this.vel);
   }
 
-  /** Steers towards average velocity of local flock */
-  align(locals: Boid[], alignment: number) {
-    if (locals.length == 0) {
-      return;
-    }
+  /** Returns steering force to match average velocity of local flock */
+  align(locals: Boid[]): Vec {
     let avgVelocity = new Vec(0, 0);
     for (let curr of locals) {
       avgVelocity = avgVelocity.add(curr.vel);
     }
-    avgVelocity = avgVelocity.scale(1 / locals.length);
-    let steering = avgVelocity.subtract(this.vel).scale(alignment);
-    this.acc = this.acc.add(steering.clamp(this.MAX_FORCE));
+    if (locals.length == 0) {
+      return avgVelocity;
+    }
+    return avgVelocity.scale(1 / locals.length).subtract(this.vel);
   }
 
-  update() {
+  /** TODO: Returns steering force to avoid border */
+  avoidBorder(width: number, height: number): Vec {
+    let steering = new Vec(0, 0);
+    if (this.pos.x < 0) {
+      steering.x = 1;
+    }
+    if (this.pos.y < 0) {
+      steering.y = 1;
+    }
+    if (this.pos.x > width) {
+      steering.x = -1;
+    }
+    if (this.pos.y > height) {
+      steering.y = -1;
+    }
+
+    return steering.subtract(this.vel);
+  }
+
+  /** Updates position of boid based on pos/vel/acc
+   * Wraps around canvas */
+  update(width: number, height: number) {
     this.vel = this.vel.add(this.acc);
     this.pos = this.pos.add(this.vel);
-    this.pos.x = mod(this.pos.x, this.board.width);
-    this.pos.y = mod(this.pos.y, this.board.height);
+    this.pos.x = mod(this.pos.x, width);
+    this.pos.y = mod(this.pos.y, height);
   }
 
-  draw() {
-    this.ctx.beginPath();
-    this.ctx.arc(this.pos.x, this.pos.y, this.RAD, 0, 2 * Math.PI);
+  /** Draws boid */
+  draw(ctx: CanvasRenderingContext2D) {
+    ctx.beginPath();
+    ctx.arc(this.pos.x, this.pos.y, this.RAD, 0, 2 * Math.PI);
 
-    this.ctx.moveTo(this.pos.x, this.pos.y);
+    ctx.moveTo(this.pos.x, this.pos.y);
     let target = this.vel
       .normalized()
       .scale(this.RAD * 2)
       .add(this.pos);
-    this.ctx.lineTo(target.x, target.y);
+    ctx.lineTo(target.x, target.y);
 
-    this.ctx.stroke();
+    ctx.stroke();
 
-    /* this.ctx.strokeStyle = "red";
-    this.ctx.beginPath();
+    ctx.strokeStyle = "red";
+    ctx.beginPath();
 
-    this.ctx.moveTo(this.pos.x, this.pos.y);
+    ctx.moveTo(this.pos.x, this.pos.y);
     target = this.acc
       .normalized()
-      .scale(this.RAD * 2)
+      .scale(this.RAD * 1)
       .add(this.pos);
-    this.ctx.lineTo(target.x, target.y);
+    ctx.lineTo(target.x, target.y);
 
-    this.ctx.stroke();
-    this.ctx.strokeStyle = "black"; */
+    ctx.stroke();
+    ctx.strokeStyle = "black";
   }
 }
 
 export class Flock extends CanvasObject {
   animator: PeriodicDrawer;
-  boids: Boid[];
+  boids: Boid[] = [];
   count: number;
-  MAX_VELOCITY = 3;
+
+  MAX_FORCE = 0.05;
+  MAX_VELOCITY = 5;
 
   COHERANCE = 0.0005;
   ALIGNMENT = 0.05;
-  SEPARATION = 0.05;
+  SEPARATION = 0.01;
 
   PERCEPTION = 70;
-  THRESHOLD = 30;
+  THRESHOLD = 25;
+
+  BORDER_FORCE = 0.05;
 
   constructor(board: CanvasBoard, count: number) {
     super(board);
     this.animator = new PeriodicDrawer(this, 120);
 
     this.count = count;
-    this.boids = [];
-    for (let i = 0; i < count; i++) {
-      this.boids.push(this.getRandomBoid());
-    }
+    this.randomizeBoids();
 
     this.animator.startRefresh();
   }
 
+  /** Replaces all boids flock with new random boids */
+  randomizeBoids() {
+    this.boids = [];
+    for (let i = 0; i < this.count; i++) {
+      this.boids.push(this.getRandomBoid());
+    }
+  }
+
+  /** Returns new boid with random position and velocity */
   getRandomBoid(): Boid {
     let dir = Math.random() * 2 * Math.PI;
     return new Boid(
-      this.board,
       new Vec(
         Math.floor(Math.random() * this.board.width * 0.8),
         Math.floor(Math.random() * this.board.height * 0.8)
@@ -133,6 +156,7 @@ export class Flock extends CanvasObject {
     );
   }
 
+  /** Gets boids around boid within certain radius */
   getLocalBoids(boid: Boid, rad: number): Boid[] {
     let localBoids: Boid[] = [];
     for (let curr of this.boids) {
@@ -143,20 +167,43 @@ export class Flock extends CanvasObject {
     return localBoids;
   }
 
+  /** Applies forces on boid and updates each accordingly */
   update() {
     this.boids.forEach((boid: Boid) => {
       boid.acc = new Vec(0, 0);
-      boid.align(this.getLocalBoids(boid, this.PERCEPTION), this.ALIGNMENT);
-      boid.cohere(this.getLocalBoids(boid, this.PERCEPTION), this.COHERANCE);
-      boid.separate(this.getLocalBoids(boid, this.THRESHOLD), this.SEPARATION);
+
+      boid.acc = boid.acc.add(
+        boid
+          .align(this.getLocalBoids(boid, this.PERCEPTION))
+          .scale(this.ALIGNMENT)
+          .clamp(this.MAX_FORCE)
+      );
+
+      boid.acc = boid.acc.add(
+        boid
+          .cohere(this.getLocalBoids(boid, this.PERCEPTION))
+          .scale(this.COHERANCE)
+          .clamp(this.MAX_FORCE)
+      );
+
+      boid.acc = boid.acc.add(
+        boid
+          .separate(this.getLocalBoids(boid, this.THRESHOLD))
+          .scale(this.SEPARATION)
+          .clamp(this.MAX_FORCE)
+      );
+
+      // boid.avoidBorder(this.board.width, this.board.height, this.BORDER_FORCE);
+
+      boid.update(this.board.width, this.board.height);
       boid.vel = boid.vel.clamp(this.MAX_VELOCITY);
-      boid.update();
     });
   }
 
+  /** Draws each boid */
   draw() {
     this.boids.forEach((boid: Boid) => {
-      boid.draw();
+      boid.draw(this.ctx);
     });
   }
 }
